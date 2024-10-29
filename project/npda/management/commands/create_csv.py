7eaf58d5f5a06ef650dcd6e9d8ff3d0c3d712a86
anchit-chandran <anchit97123@gmail.com>
2024-10-29 09:07:04 +0000

    init create csv
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

---
+++ project/npda/management/commands/create_csv.py
@@ -0,0 +1,277 @@
+"""TODO:
+    - [ ] Move constants to a separate file. Currently importing from `seed_submission.py`.
+    - [ ] Generalise the parsing of inputs and share between this and `seed_submission.py`.
+
+Generate a CSV using data generator.
+
+Default behavior of data generator is creating VALID visits - CSV.
+
+Example use:
+
+    python manage.py create_csv \
+        --pts=5 \
+        --visits="CDCD DHPC ACDC CDCD" \
+        --hb_target=T
+
+    Options:
+    
+    --pts (int, required):
+        The number of pts to seed for this csv file. (NOTE: resulting rows will be pts * visits
+        )
+
+    --visits (str, required):
+        A string encoding the VisitTypes each patient should have. Use
+        visit type abbreviations. Can use whitespace (ignored)
+        (e.g., "CDCD DHPC ACDC CDCD").
+        Each patient will have associated Visits in the sequence provided,
+        evenly spread throughout the audit year's quarters, randomly within
+        each quarter.
+
+        Visit type options:
+            - C (CLINIC)
+            - A (ANNUAL_REVIEW)
+            - D (DIETICIAN)
+            - P (PSYCHOLOGY)
+            - H (HOSPITAL_ADMISSION)
+
+    --hb_target (str, required):
+        Character setting for HbA1c target range per visit:
+            - T (TARGET)
+            - A (ABOVE)
+            - W (WELL_ABOVE)
+    
+    --submission_date (str, optional):
+        The submission date in YYYY-MM-DD format. Defaults to today. This
+        date is used to set the audit period's start and end dates, and visit
+        values e.g. diabetes diagnosis date.
+
+Implementation notes: 
+
+    We can use the `FakePatientCreator`'s `.build()` methods to generate Python object stubs of Patients and Visits. We then use pandas to concatenate these values into the csv.
+    
+    Factory `.build()` will not create related objects (but is significantly quicker). At the end,
+    need to additionally add the `Transfer` column values manually.
+"""
+
+from datetime import datetime
+
+from django.utils import timezone
+from django.core.management.base import BaseCommand
+import pandas as pd
+import numpy as np
+
+from project.constants.csv_headings import CSV_HEADINGS
+from project.npda.general_functions.audit_period import (
+    get_audit_period_for_date,
+)
+from project.npda.general_functions.data_generator_extended import (
+    AgeRange,
+    FakePatientCreator,
+    HbA1cTargetRange,
+    VisitType,
+)
+from project.npda.models import (
+    NPDAUser,
+    Patient,
+    Submission,
+    OrganisationEmployer,
+)
+from project.npda.management.commands.seed_submission import letter_name_map, hb_target_map, CYAN, RESET
+
+class Command(BaseCommand):
+    help = "Creates a csv file that can be uploaded to the NPDA platform."
+
+    def print_success(self, message: str):
+        self.stdout.write(self.style.SUCCESS(message))
+
+    def print_info(self, message: str):
+        self.stdout.write(self.style.WARNING(message))
+
+    def print_error(self, message: str):
+        self.stdout.write(self.style.ERROR(f"ERROR: {message}"))
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--pts",
+            type=int,
+            required=True,
+            help="Number of patients to seed.",
+        )
+        parser.add_argument(
+            "--visits",
+            type=str,
+            required=True,
+            help="Visit types (e.g., 'CDCD DHPC ACDC CDCD'). Can have whitespaces, these will be "
+            "ignored",
+        )
+        parser.add_argument(
+            "--hb_target",
+            type=str,
+            required=True,
+            choices=["T", "A", "W"],
+            help="HBA1C Target range for visit seeding.",
+        )
+        parser.add_argument(
+            "--submission_date",
+            type=str,
+            help="Submission date in YYYY-MM-DD format (optional, defaults to today).",
+        )
+    
+    def handle(self, *args, **options):
+
+        if not (parsed_values := self._parse_values_from_options(**options)):
+            return
+
+        audit_start_date = parsed_values["audit_start_date"]
+        audit_end_date = parsed_values["audit_end_date"]
+        n_pts_to_seed = parsed_values["n_pts_to_seed"]
+        hba1c_target = parsed_values["hba1c_target"]
+        visits = parsed_values["visits"]
+        visit_types = parsed_values["visit_types"]
+        submission_date = parsed_values["submission_date"]
+        age_range = AgeRange.AGE_11_15
+        
+        # Print out the parsed values
+        self.print_info(
+            f"Using submission_date: {CYAN}{submission_date}{RESET}\n"
+            f"Audit period: Start Date - {CYAN}{audit_start_date}{RESET}, "
+            f"End Date - {CYAN}{audit_end_date}{RESET}\n"
+        )
+        self.print_info(
+            f"Number of rows to seed: {CYAN}{n_pts_to_seed}{RESET}\n"
+        )
+        formatted_visits = "\n    ".join(
+            f"{CYAN}{visit_type}{RESET}"
+            for visit_type in self._map_visit_type_letters_to_names(
+                visits
+            ).split("\n")
+        )
+        self.print_info(f"Visit types provided:\n    {formatted_visits}\n")
+        self.print_info(f"HbA1c target range: {CYAN}{hba1c_target}{RESET}\n")
+        
+        # Start csv logic
+        
+        # First initialise FakePatientCreator object
+        fake_patient_creator = FakePatientCreator(
+            audit_start_date=audit_start_date,
+            audit_end_date=audit_end_date,
+        )
+        
+        # Build pt stubs
+        new_pts = fake_patient_creator.build_fake_patients(
+            n=n_pts_to_seed,
+            age_range=age_range,
+        )
+        
+        # For each pt, add visits
+        new_visits = fake_patient_creator.build_fake_visits(
+            patients=new_pts,
+            age_range=age_range,
+            hb1ac_target_range=hba1c_target,
+            visit_types=visit_types,
+        )
+        
+        # `CSV_HEADINGS` is a tuple for csv headings and model fields
+        # first create a map { model : {csv_heading, model_field} }
+        for csv_heading in CSV_HEADINGS:
+        
+        
+        from pprint import pprint
+        for v in new_visits:
+            pprint(v.__dict__)
+        
+    
+    def _parse_values_from_options(self, **options):
+
+        # Handle submission_date with default to today's date if not provided
+        submission_date_str = options.get("submission_date")
+        if submission_date_str:
+            try:
+                submission_date = timezone.make_aware(
+                    datetime.strptime(submission_date_str, "%Y-%m-%d")
+                ).date()
+            except ValueError:
+                self.print_error(
+                    "Invalid submission_date format. Use YYYY-MM-DD."
+                )
+                return
+        else:
+            submission_date = timezone.now().date()
+
+        audit_start_date, audit_end_date = get_audit_period_for_date(
+            submission_date
+        )
+
+        # Number of patients to seed (pts)
+        n_pts_to_seed = options["pts"]
+
+        # Visit types
+        visits: str = options["visits"]
+        # Map to actual VisitType
+        # NOTE: `_map_visit_type_letters_to_names` already did some basic validation
+        visit_types = list(
+            map(
+                lambda letter: letter_name_map[letter],
+                visits.replace(" ", ""),
+            )
+        )
+
+        # hba1c target
+        hba1c_target = hb_target_map[options["hb_target"]]
+
+        return {
+            "n_pts_to_seed": n_pts_to_seed,
+            "audit_start_date": audit_start_date,
+            "audit_end_date": audit_end_date,
+            "hba1c_target": hba1c_target,
+            "visits": visits,
+            "visit_types": visit_types,
+            "submission_date": submission_date,
+        }
+    
+    def _map_visit_type_letters_to_names(self, vt_letters: str) -> str:
+        rendered_vt_names: list[str] = []
+
+        for letter in vt_letters:
+            if letter == " ":
+                rendered_vt_names.append("\n\t")
+                continue
+            if letter.upper() not in "CADPH":
+                self.print_error("INVALID VISIT TYPE LETTER: " + letter)
+
+            rendered_vt_names.append(f"\n\t{letter_name_map[letter]}")
+
+        return "".join(rendered_vt_names)
+
+    def _get_map_model_csv_heading_field(self)-> dict:
+        """Generates dict that looks like:
+        
+        {
+            'Patient': {
+                'nhs_number': 'NHS Number',
+                'date_of_birth': 'Date of Birth',  
+                ...
+            },
+            'Visit': {
+                'visit_type': 'Visit Type',
+                'visit_date': 'Visit/Appointment Date',
+                ...
+            },
+            'Transfer': {
+                'date_leaving_service': 'Date Leaving Service',
+                ...
+        }
+        """
+        
+        map_model_csv_heading_field = {}
+        
+        for csv_heading in CSV_HEADINGS:
+            model = csv_heading.model
+            csv_heading = csv_heading.csv_heading
+            model_field = csv_heading.model_field
+            map_model_csv_heading_field[model] = {
+                "csv_heading": csv_heading,
+                "model_field": model_field
+            }
+        
+        return map_model_csv_heading_field
