150187180e67ff64c4837ddc417e191b48d3b73a
reecehill <29638128+reecehill@users.noreply.github.com>
2024-11-11 19:17:53 +0000

    refactor download_csv

--- project/npda/general_functions/csv_download.py
+++ project/npda/general_functions/csv_download.py
@@ -2,6 +2,11 @@ from django.apps import apps
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 
+def download_file(file_path, file_name):
+    with open(file_path, "rb") as f:
+        response = HttpResponse(f.read(), content_type="text/csv")
+        response["Content-Disposition"] = f'attachment; filename="{file_name}"'
+        return response
 
 def download_csv(request, submission_id):
     """
@@ -12,7 +17,16 @@ def download_csv(request, submission_id):
     file_path = submission.csv_file.path
     file_name = submission.csv_file.name.split("/")[-1]
 
-    with open(file_path, "rb") as f:
-        response = HttpResponse(f.read(), content_type="text/csv")
-        response["Content-Disposition"] = f'attachment; filename="{file_name}"'
-        return response
+    return download_file(file_path, file_name)
+
+def download_xlsx(request, submission_id):
+    """
+    Download a XLSX file.
+    NB: This repurposes download_csv with a simple file rename.
+    """
+    Submission = apps.get_model(app_label="npda", model_name="Submission")
+    submission = get_object_or_404(Submission, id=submission_id)
+    file_path = submission.csv_file.path.replace('.csv','.xlsx')
+    file_name = submission.csv_file.name.split("/")[-1].replace('.csv','.xlsx')
+
+    return download_file(file_path, file_name)
