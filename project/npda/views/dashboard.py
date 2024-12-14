22bd681651018bb877fb3000efdb53b5254eee30
eatyourpeas <simon@eatyourpeas.co.uk>
2024-12-14 12:44:02 +0000

    Map render in htmx partial view crashing on dashboard Fixes #422

--- project/npda/views/dashboard.py
+++ project/npda/views/dashboard.py
@@ -1,12 +1,12 @@
 # Python imports
-from collections import defaultdict
+from collections import Counter, defaultdict
 import datetime
 import json
 import logging
 from datetime import date
+from typing import Literal
 
 import plotly.graph_objects as go
-import plotly.io as pio
 
 # Django imports
 from django.apps import apps
@@ -15,13 +15,17 @@ from django.http import HttpResponseBadRequest, HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.db.models import QuerySet, Count
 
-from project.constants.colors import RCPCH_DARK_BLUE, RCPCH_MID_GREY, RCPCH_PINK
+from project.constants.colors import *
 from project.constants.diabetes_types import DIABETES_TYPES
+from project.constants.ethnicities import ETHNICITIES
+from project.constants.sex_types import SEX_TYPE
+from project.constants.types.kpi_types import KPIRegistry
 from project.npda.general_functions.model_utils import print_instance_field_attrs
 from project.npda.general_functions.quarter_for_date import retrieve_quarter_for_date
 from project.npda.models.paediatric_diabetes_unit import (
     PaediatricDiabetesUnit as PaediatricDiabetesUnitClass,
 )
+from project.npda.models.patient import Patient
 
 
 # HTMX imports
@@ -97,33 +101,38 @@ def dashboard(request):
     # From this, gather specific chart data required
 
     # Total eligible patients stratified by diabetes type
-    total_eligible_patients_queryset: QuerySet = kpi_calculations_object[
-        "calculated_kpi_values"
-    ]["kpi_1_total_eligible"]["patient_querysets"]["eligible"]
-    eligible_pts_diabetes_type_counts = total_eligible_patients_queryset.values(
-        "diabetes_type"
-    ).annotate(count=Count("diabetes_type"))
-    eligible_pts_diabetes_type_value_counts = defaultdict(int)
-    for item in eligible_pts_diabetes_type_counts:
-        diabetes_type = item["diabetes_type"]
-        count = item["count"]
-
-        # These are T1/T2DM types
-        if diabetes_type == 1:
-            diabetes_type_str = "T1DM"
-            eligible_pts_diabetes_type_value_counts[diabetes_type_str] += count
-        elif diabetes_type == 2:
-            diabetes_type_str = "T2DM"
-            eligible_pts_diabetes_type_value_counts[diabetes_type_str] += count
-        else:
-            # Count as 'Other rare forms'
-            diabetes_type_str = "Other rare forms"
-            eligible_pts_diabetes_type_value_counts[diabetes_type_str] += count
-
-    # Convert to percentages
-    eligible_pts_diabetes_type_value_counts = convert_value_counts_dict_to_pct(
-        eligible_pts_diabetes_type_value_counts
+    total_eligible_pts_diabetes_type_value_counts = (
+        get_total_eligible_pts_diabetes_type_value_counts(
+            eligible_pts_queryset=kpi_calculations_object["calculated_kpi_values"][
+                "kpi_1_total_eligible"
+            ]["patient_querysets"]["eligible"]
+        )
     )
+
+    # Patient characteristics -> KPI [4, 5, 6, 8, 9, 10, 11, 12]
+    pt_characteristics_value_counts = get_pt_characteristics_value_counts_pct(
+        calculate_kpis.kpi_name_registry,
+        kpi_calculations_object["calculated_kpi_values"],
+    )
+    # A single chart has 5 figures -> based on pct, return the number of figures coloured
+    pt_characteristics_value_counts_with_figure_counts = (
+        add_number_of_figures_coloured_for_chart(pt_characteristics_value_counts)
+    )
+
+    # Sex
+    pt_sex_value_counts, pt_ethnicity_value_counts, pt_imd_value_counts = (
+        get_pt_demographic_value_counts(
+            all_eligible_pts_queryset=kpi_calculations_object["calculated_kpi_values"][
+                "kpi_1_total_eligible"
+            ]["patient_querysets"]["eligible"]
+        )
+    )
+    # Convert to pcts
+    pt_sex_value_counts_pct = convert_value_counts_dict_to_pct(pt_sex_value_counts)
+    pt_ethnicity_value_counts_pct = convert_value_counts_dict_to_pct(
+        pt_ethnicity_value_counts
+    )
+    pt_imd_value_counts_pct = convert_value_counts_dict_to_pct(pt_imd_value_counts)
 
     # Gather other context vars
     current_date = date.today()
@@ -147,10 +156,12 @@ def dashboard(request):
         "current_quarter": current_quarter,
         "days_remaining_until_audit_end_date": days_remaining_until_audit_end_date,
         "charts": {
-            # Converting this to a dict for easier access in the template
             "total_eligible_patients_stratified_by_diabetes_type": {
-                "data": json.dumps(eligible_pts_diabetes_type_value_counts),
-                "labels": list(eligible_pts_diabetes_type_value_counts.keys()),
+                "data": json.dumps(total_eligible_pts_diabetes_type_value_counts),
+                "labels": list(total_eligible_pts_diabetes_type_value_counts.keys()),
+            },
+            "pt_characteristics_value_counts": {
+                "data": pt_characteristics_value_counts_with_figure_counts,
             },
             "map": json.dumps(
                 dict(
@@ -158,6 +169,15 @@ def dashboard(request):
                     selected_audit_year=selected_audit_year,
                 )
             ),
+            "pt_sex_value_counts_pct": {
+                "data": json.dumps(pt_sex_value_counts_pct),
+            },
+            "pt_ethnicity_value_counts_pct": {
+                "data": json.dumps(pt_ethnicity_value_counts_pct),
+            },
+            "pt_imd_value_counts_pct": {
+                "data": json.dumps(pt_imd_value_counts_pct),
+            },
         },
         # Defaults for htmx partials
         "default_pt_level_menu_text": default_pt_level_menu_text,
@@ -202,9 +222,6 @@ def get_waffle_chart_partial(request):
     if not request.htmx:
         return HttpResponseBadRequest("This view is only accessible via HTMX")
 
-    if not request.GET:
-        return HttpResponseBadRequest("No data provided")
-
     # Fetch data from query parameters
     data = {}
     for key, value in request.GET.items():
@@ -222,7 +239,26 @@ def get_waffle_chart_partial(request):
 
     # Prepare waffle chart
     # TODO: ADD IN A BUNCH OF COLORS HERE. ?COULD SPECIFY COLORS IN GET REQUEST
-    colours = [RCPCH_DARK_BLUE, RCPCH_PINK, RCPCH_MID_GREY][: len(data)]
+    colours = [
+        RCPCH_DARK_BLUE,
+        RCPCH_PINK,
+        RCPCH_MID_GREY,
+        RCPCH_CHARCOAL_DARK,
+        RCPCH_RED,
+        RCPCH_ORANGE,
+        RCPCH_YELLOW,
+        RCPCH_STRONG_GREEN,
+        RCPCH_AQUA_GREEN,
+        RCPCH_PURPLE,
+        RCPCH_PURPLE_LIGHT_TINT2,
+        RCPCH_PURPLE_DARK_TINT,
+        RCPCH_RED_LIGHT_TINT3,
+        RCPCH_ORANGE_LIGHT_TINT3,
+        RCPCH_STRONG_GREEN_LIGHT_TINT3,
+        RCPCH_AQUA_GREEN_LIGHT_TINT3,
+        RCPCH_ORANGE_LIGHT_TINT3,
+        RCPCH_DARK_GREY,
+    ][: len(data)]
 
     # Create Plotly waffle chart
     GRID_SIZE = 10  # 10x10 grid
@@ -259,9 +295,14 @@ def get_waffle_chart_partial(request):
                 x=[square["x"]],
                 y=[square["y"]],
                 mode="markers",
-                marker=dict(size=20, color=square["colour"], symbol="square"),
+                marker=dict(
+                    size=25,
+                    color=square["colour"],
+                    symbol="square",
+                ),
                 name=square["category"],
                 showlegend=False,
+                hoverinfo="skip",
             )
         )
 
@@ -274,6 +315,7 @@ def get_waffle_chart_partial(request):
                 mode="markers",
                 marker=dict(size=15, color=colours[idx], symbol="square"),
                 name=f"{pct}% {label}",
+                hoverinfo="skip",
             )
         )
 
@@ -290,7 +332,10 @@ def get_waffle_chart_partial(request):
     chart_html = fig.to_html(
         full_html=False,
         include_plotlyjs=False,
-        config={"displayModeBar": False},
+        config={
+            "displayModeBar": False,
+            "staticPlot": True,
+        },
     )
     return render(
         request, "dashboard/waffle_chart_partial.html", {"chart_html": chart_html}
@@ -355,6 +400,198 @@ def get_map_chart_partial(request):
             "aggregated_distances": aggregated_distances,
         },
     )
+
+
+@login_and_otp_required()
+def get_colored_figures_chart_partial(
+    request,
+    colored: int,
+    total_figures: int,
+):
+
+    if not request.htmx:
+        return HttpResponseBadRequest("This view is only accessible via HTMX")
+
+    # Get a list of booleans to determine which figures are colored (to iterate easily in the
+    # template)
+    is_colored = [True if i < colored else False for i in range(total_figures)]
+
+    return render(
+        request,
+        "dashboard/colored_figures_chart_partial.html",
+        context={
+            "is_colored": is_colored,
+        },
+    )
+
+
+def get_total_eligible_pts_diabetes_type_value_counts(
+    eligible_pts_queryset: QuerySet,
+) -> dict:
+    """Gets value counts dict for total eligible patients stratified by diabetes type"""
+
+    eligible_pts_diabetes_type_counts = eligible_pts_queryset.values(
+        "diabetes_type"
+    ).annotate(count=Count("diabetes_type"))
+    eligible_pts_diabetes_type_value_counts = defaultdict(int)
+    for item in eligible_pts_diabetes_type_counts:
+        diabetes_type = item["diabetes_type"]
+        count = item["count"]
+
+        # These are T1/T2DM types
+        if diabetes_type == 1:
+            diabetes_type_str = "T1DM"
+            eligible_pts_diabetes_type_value_counts[diabetes_type_str] += count
+        elif diabetes_type == 2:
+            diabetes_type_str = "T2DM"
+            eligible_pts_diabetes_type_value_counts[diabetes_type_str] += count
+        else:
+            # Count as 'Other rare forms'
+            diabetes_type_str = "Other rare forms"
+            eligible_pts_diabetes_type_value_counts[diabetes_type_str] += count
+
+    # Convert to percentages
+    eligible_pts_diabetes_type_value_counts = convert_value_counts_dict_to_pct(
+        eligible_pts_diabetes_type_value_counts
+    )
+
+    return eligible_pts_diabetes_type_value_counts
+
+
+def get_pt_characteristics_value_counts_pct(
+    kpi_name_registry: KPIRegistry,
+    kpi_calculations_object: dict,
+) -> dict[
+    Literal["care", "died_or_transitioned", "comorbidity_and_testing"],
+    dict[Literal["total_eligible", "total_ineligible", "pct"], int],
+]:
+    """Gets value counts dict for:
+
+    care
+    - age_gte_12yo (KPI4)
+    - complete_year_of_care (KPI5)
+    - age_gte_12yo_and_complete_year_of_care (KPI6)
+
+    died_or_transitioned
+    - died (KPI8)
+    - transitioned (KPI9)
+
+    comorbidity_and_testing
+    - coeliac (KPI10)
+    - thyroid (kpi11)
+    - ketone_testing (KPI12)
+
+    NOTE: rounds DOWN (convert float to int) for percentage calculation
+    """
+    # Get attribute names and labels
+    relevant_kpis = [4, 5, 6, 8, 9, 10, 11, 12]
+    kpi_attr_names = [
+        kpi_name_registry.get_attribute_name(kpi) for kpi in relevant_kpis
+    ]
+
+    value_counts = defaultdict(lambda: {"count": 0, "total": 0, "pct": 0})
+    # These are all just counts so only total_eligble and total_ineligible have values
+    for kpi_attr in kpi_attr_names:
+
+        total_eligible = kpi_calculations_object[kpi_attr]["total_eligible"]
+        total_ineligible = kpi_calculations_object[kpi_attr]["total_ineligible"]
+
+        # Need all 3 for front end chart
+        value_counts[kpi_attr]["count"] = total_eligible
+        value_counts[kpi_attr]["total"] = total_eligible + total_ineligible
+        value_counts[kpi_attr]["pct"] = (
+            int(total_eligible / value_counts[kpi_attr]["total"] * 100)
+            if value_counts[kpi_attr]["total"] > 0
+            else 0
+        )
+
+    # Now put into the 3 categories
+    categories_vc = defaultdict(dict)
+    for kpi in [4, 5, 6]:
+        kpi_attr = kpi_name_registry.get_attribute_name(kpi)
+        categories_vc["care"][kpi_attr] = value_counts[kpi_attr]
+
+    for kpi in [8, 9]:
+        kpi_attr = kpi_name_registry.get_attribute_name(kpi)
+        categories_vc["died_or_transitioned"][kpi_attr] = value_counts[kpi_attr]
+
+    for kpi in [10, 11, 12]:
+        kpi_attr = kpi_name_registry.get_attribute_name(kpi)
+        categories_vc["comorbidity_and_testing"][kpi_attr] = value_counts[kpi_attr]
+
+    return dict(categories_vc)
+
+
+def get_pt_demographic_value_counts(
+    all_eligible_pts_queryset: QuerySet[Patient],
+) -> tuple[
+    dict[Literal["Female", "Male", "Unknown"], int],
+    dict[str, int],
+    dict[
+        Literal[
+            1,
+            2,
+            3,
+            4,
+            5,
+        ],
+        int,
+    ],
+]:
+    """Get value counts for pt demographics:
+
+    - sex
+    - ethnicity
+    - imd
+    """
+
+    all_values = all_eligible_pts_queryset.values(
+        "sex", "ethnicity", "index_of_multiple_deprivation_quintile"
+    )
+    sex_map = dict(SEX_TYPE)
+    sex_counts = Counter(sex_map[item["sex"]] for item in all_values)
+    ethnicity_map = dict(ETHNICITIES)
+    ethnicity_counts = Counter(ethnicity_map[item["ethnicity"]] for item in all_values)
+    imd_map = {
+        1: "1st Quintile",
+        2: "2nd Quintile",
+        3: "3rd Quintile",
+        4: "4th Quintile",
+        5: "5th Quintile",
+    }
+    imd_counts = Counter(
+        imd_map[item["index_of_multiple_deprivation_quintile"]] for item in all_values
+    )
+
+    return (
+        sex_counts,
+        ethnicity_counts,
+        imd_counts,
+    )
+
+
+def add_number_of_figures_coloured_for_chart(
+    value_counts_dict: dict[
+        Literal["care", "died_or_transitioned", "comorbidity_and_testing"],
+        dict[Literal["total_eligible", "total_ineligible", "pct"], int],
+    ],
+    n_figures_total: int = 5,
+) -> dict[
+    Literal["total_eligible", "total_ineligible", "pct", "figures_coloured"], int
+]:
+    """
+    Add number of figures coloured to a value counts dict
+    """
+    # divisor is 100 / n_figures_total
+    divisor = 100 / n_figures_total
+
+    for category, vcs in value_counts_dict.items():
+        for key, value in vcs.items():
+            value_counts_dict[category][key]["figures_coloured"] = int(
+                value["pct"] / divisor
+            )
+
+    return dict(value_counts_dict)
 
 
 def convert_value_counts_dict_to_pct(value_counts_dict: dict):
